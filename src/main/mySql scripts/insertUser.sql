e94df903b87c2a430e6a33b33d28634ce5f02fd9
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-01 00:47:07 +0100

    Add mysql scripts

---
+++ src/main/mySql scripts/insertUser.sql
@@ -0,0 +1,2 @@
+INSERT INTO user VALUES (1, 200, "theEmail@email.com","456",0,0,0,0,"456","aUser1",null);
+INSERT INTO user VALUES (2, 100, "anEmail@email.com","123",0,0,0,0,"123","aUser2",null);
---
+++ src/main/mySql scripts/user.sql
@@ -0,0 +1,2 @@
+SELECT * FROM db_sehm.user;
+DELETE FROM db_sehm.user WHERE id = 3
