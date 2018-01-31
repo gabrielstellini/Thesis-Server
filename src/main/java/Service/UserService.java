0bd824aa4354b8971eed42e9a8256be8e5b86fd1
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-01 00:48:41 +0100

    Update userService

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -3,7 +3,10 @@ package Service;
 import Model.DatabaseEntities.User;
 import Model.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.StreamSupport;
 
 @Service
 public class UserService {
@@ -14,11 +17,34 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User findByUsername(String userName){
-        return userRepository.findByUsername(userName);
+    public User findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public User[] findAllBySimilarUsername(String username){
+        String regex = "(?i)(.*)" + username + "(.*)";
+
+        Iterable<User> usersIterable = userRepository.findAll();
+        User[] users = StreamSupport
+                .stream(usersIterable.spliterator(), false)
+                .filter(user -> user.getUsername().matches(regex))
+                .toArray(User[]::new);
+        return users;
     }
 
     public Iterable<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public void save(User user){
+        if(user.getPassword() != null) {
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        }
+
+        //TODO: more robust checks for whether a user is valid
+        if(user.getUsername() != null && (user.getPassword() != null || user.getGoogleId() != null)) {
+            userRepository.save(user);
+        }
     }
 }
