e84ff03bb0b4d6fe9ecfa20d93a21db175a5cd82
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-01 00:44:32 +0100

    Add error handling

--- src/main/java/Model/EntityToDto.java
+++ src/main/java/Model/EntityToDto.java
@@ -4,7 +4,12 @@ import org.springframework.beans.BeanUtils;
 
 public abstract class EntityToDto <D, E>{
     public D toDto(E entity, D dto) {
-        BeanUtils.copyProperties(entity, dto);
-        return dto;
+        if(entity != null & dto != null) {
+            BeanUtils.copyProperties(entity, dto);
+            return dto;
+        }
+        else {
+            return null;
+        }
     }
 }
