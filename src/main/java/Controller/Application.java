69a41e72ab913bc86975df5eb56604f122b809e5
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-02-02 20:05:05 +0100

    Add OAuth configuration

--- src/main/java/Controller/Application.java
+++ src/main/java/Controller/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableOAuth2Sso // Without this, basic authentication is invoked
 public class Application {
 
     public static void main(String[] args) {
