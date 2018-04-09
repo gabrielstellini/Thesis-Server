3fcc4563f4dc2fa520309d12b63fc7d5883acf66
Gabriel Stellini <gs703@live.mdx.ac.uk>
2018-04-09 23:26:22 +0200

    Add score saving

--- src/main/java/Service/ScoreService.java
+++ src/main/java/Service/ScoreService.java
@@ -22,4 +22,8 @@ public class ScoreService {
         User user = userService.findByGoogleId(googleId);
         return scoreRepository.findByUser(user);
     }
+
+    public void save(Score score){
+        scoreRepository.save(score);
+    }
 }
